b9201f9b738f90af6d86a533a0fff1efb0d89953
Paolo <pcezar87@gmail.com>
2025-10-18 23:09:30 -0300

    semicolon

--- src/app/services/graphql.ts
+++ src/app/services/graphql.ts
@@ -54,7 +54,7 @@ const getAuthorizationToken = async (
 
   if (!token) {
     const cookieHeader = req.headers.get("cookie");
-    console.log({ cookieHeader })
+    console.log({ cookieHeader });
     if (cookieHeader) {
       // First try the full cookie header shim (existing fallback)
       try {
